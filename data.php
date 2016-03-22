b9c80176d832f73c3c67d250d3fcce1cf530f478
Jason Grant <jason@flexewebs.com>
2016-03-22 15:24:39 +0000

    Added tracking code and more words.

--- data.php
+++ data.php
@@ -13,7 +13,7 @@
 
 $style          = array('one', 'two', 'three', 'four', 'five', 'six');
 $determiner     = array('The', 'A', 'An');
-$noun           = array('design', 'Information Architecture', 'experience', 'blueprint', 'solution', 'problem', 'stakeholder', 'designer', 'idea', 'facts', 'Virtual Reality', 'research', 'personas', 'process', 'brand', 'visuals', 'wireframe', 'sketch', 'service', 'user journey', 'channel', 'social media', 'medium', 'web', 'app', 'technology', 'business', 'blockchain', 'IoT', 'Artificial Intelligence', 'Network', 'product', 'tools');
+$noun           = array('design', 'Information Architecture', 'experience', 'blueprint', 'solution', 'problem', 'stakeholder', 'designer', 'idea', 'facts', 'Virtual Reality', 'research', 'personas', 'process', 'brand', 'visuals', 'wireframe', 'sketch', 'service', 'user journey', 'channel', 'social media', 'medium', 'web', 'app', 'technology', 'business', 'blockchain', 'IoT', 'Artificial Intelligence', 'Network', 'product', 'tools', 'ecosystem', 'system', 'dream', 'faith', 'gut feeling', 'hunch', 'app', 'platform', 'marketing', 'engagement', 'strategy', 'leadership', 'choice', 'thing', 'computer', 'smartphone', 'device', 'taxonomy');
 $verb           = array('discuss', 'will', 'think', 'wonder', 'believe', 'design', 'develop', 'iterate', 'scrap', 'undermine', 'dislike', 'like',);
 $preposition    = array('beneath', 'to', 'up', 'as', 'in', 'for', 'above', 'into', 'over', 'past', 'like', 'near', 'next');
 
